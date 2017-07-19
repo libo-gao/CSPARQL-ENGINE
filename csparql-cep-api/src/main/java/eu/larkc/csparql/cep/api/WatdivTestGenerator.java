7493d7124226eaaba6ef3acc3c6a6f709a164757
nosrepus <nosrepus@gmail.com>
2017-07-18 22:03:07 -0400

    add offer in player

--- csparql-cep-api/src/main/java/eu/larkc/csparql/cep/api/WatdivTestGenerator.java
+++ csparql-cep-api/src/main/java/eu/larkc/csparql/cep/api/WatdivTestGenerator.java
@@ -3,10 +3,7 @@ package eu.larkc.csparql.cep.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 
 import static java.lang.Double.min;
@@ -37,42 +34,50 @@ public class WatdivTestGenerator extends RdfStream implements Runnable {
         return Double.parseDouble(ss.split("\t")[3]);
     }
 
+    private double getMin(double a, double b, double c){
+        return min(a, min(b, c));
+    }
 
     public void run() {
         FileInputStream file_like = null;
         FileInputStream file_purchase = null;
         FileInputStream file_review = null;
+        FileInputStream file_offer = null;
         try {
             file_like = new FileInputStream("data/scale50/like_stream.txt");
             file_purchase = new FileInputStream("data/scale50/purchase.txt");
             file_review = new FileInputStream("data/scale50/review.txt");
+            file_offer = new FileInputStream("data/scale50/offer.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         BufferedReader in_like = new BufferedReader(new InputStreamReader(file_like));
         BufferedReader in_purchase = new BufferedReader(new InputStreamReader(file_purchase));
         BufferedReader in_review = new BufferedReader(new InputStreamReader(file_review));
+        BufferedReader in_offer = new BufferedReader(new InputStreamReader(file_offer));
         try {
             String curr_like = in_like.readLine();
             String curr_purchase = in_purchase.readLine();
             String curr_review = in_review.readLine();
+            String curr_offer = in_offer.readLine();
 
             double time_like = getTime(curr_like);
             double time_purchase = getTime(curr_purchase);
             double time_review = getTime(curr_review);
+            double time_offer = getTime(curr_offer);
 
             ArrayList<String> subjects = new ArrayList<String>();
             ArrayList<String> predicates = new ArrayList();
             ArrayList<String> objects = new ArrayList();
 
-            while (curr_like.length() != 0 || curr_purchase.length() != 0 || curr_review.length() != 0) {
+            while (curr_like.length() != 0 || curr_purchase.length() != 0 || curr_review.length() != 0|| curr_offer.length() != 0) {
 
                 //the time used to prepare the data is around 0.3 millisecond
 
                 //long t1 = System.nanoTime();
                 //System.out.printf("starttime:"+'\t'+System.nanoTime()+'\n');
 
-                if (time_like <= min(time_purchase, time_review)) {
+                if (time_like <= getMin(time_purchase, time_review, time_offer)) {
                     //System.out.printf(curr_like + '\n');
 
                     String[] items = curr_like.split("\t");
@@ -96,7 +101,7 @@ public class WatdivTestGenerator extends RdfStream implements Runnable {
                     } else {
                         time_like = getTime(curr_like);
                     }
-                } else if (time_purchase <= min(time_like, time_review)) {
+                } else if (time_purchase <= getMin(time_like, time_review, time_offer)) {
                     //System.out.printf(curr_purchase + '\n');
 
                     String[] items = curr_purchase.split("\t");
@@ -118,7 +123,7 @@ public class WatdivTestGenerator extends RdfStream implements Runnable {
 
                     if (curr_purchase.length() == 0) time_purchase = 2.0;
                     else time_purchase = getTime(curr_purchase);
-                } else if (time_review <= min(time_like, time_purchase)) {
+                } else if (time_review <= getMin(time_like, time_purchase, time_offer)) {
                     //System.out.printf(curr_review + '\n');
 
                     String[] items = curr_review.split("\t");
@@ -140,6 +145,24 @@ public class WatdivTestGenerator extends RdfStream implements Runnable {
 
                     if (curr_review.length() == 0) time_review = 2.0;
                     else time_review = getTime(curr_review);
+                } else if (time_offer <= getMin(time_like, time_purchase, time_review)){
+                    String[] items = curr_offer.split("\t");
+                    subjects.add(items[0]);
+                    predicates.add(items[1]);
+                    objects.add(items[2]);
+
+                    curr_offer = in_offer.readLine();
+
+                    while (curr_offer.length() != 0 && time_offer == getTime(curr_offer)) {
+                        items = curr_offer.split("\t");
+                        subjects.add(items[0]);
+                        predicates.add(items[1]);
+                        objects.add(items[2]);
+
+                        curr_offer = in_offer.readLine();
+                    }
+                    if (curr_offer.length() ==0) time_offer = 2.0;
+                    else time_offer = getTime(curr_offer);
                 }
                 //System.out.printf("endtime:"+'\t'+System.nanoTime()+'\n'+'\n');
                 //long t2 = System.nanoTime();
